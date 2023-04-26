37bd0164acfa2d0e3b870ebe66660c51669c624c
Samip Shrestha <javasamip@gmail.com>
2023-04-27 01:49:24 +0545

    refact: app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import { useState } from "react";
-
-import ListGroup from "./components/ListGroup";
-import AlertComponent from "./components/Alert";
-import Button from "@mui/material/Button";
-
 import "./App.css";
 import DrawerMenu from "./components/DrawerMenu/DrawerMenu";
+import HomePage from "./views/HomePage/HomePage";
 
 function App() {
-  const items = ["Pizza", "Burger", "Pasta", "Salad", "Sandwich"];
-
-  const onSelected = (item: string) => {
-    console.log(item, "From parent");
-  };
-
-  const [showAlert, setShow] = useState(false);
-
   return (
-    <div>
-      <ListGroup items={items} heading="Lunch Menu" onSelected={onSelected} />
-      {showAlert && <AlertComponent
-        show={showAlert}
-        type="success"
-        close={() => setShow(false)}
-      >
-        Simple Alert
-      </AlertComponent>}
-
-      <Button variant="outlined" onClick={() => setShow(true)}>Alert Me</Button>
-      <DrawerMenu></DrawerMenu>
-    </div>
+    <DrawerMenu>
+      <HomePage></HomePage>
+    </DrawerMenu>
   );
 }
 
