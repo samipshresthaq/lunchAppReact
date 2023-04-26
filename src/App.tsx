d7a9bd03573edf125d9b969bab0e0e45c8e0ef1c
Samip Shrestha <javasamip@gmail.com>
2023-04-27 02:25:55 +0545

    feat: pass menu list as drawer menu prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 import DrawerMenu from "./components/DrawerMenu/DrawerMenu";
 import HomePage from "./views/HomePage/HomePage";
+import MenuList from "./components/MenuList/MenuList";
 
 function App() {
+  const [drawerState, setDrawerState] = useState(false);
+
   return (
-    <DrawerMenu>
+    <DrawerMenu
+      menuList={<MenuList drawerState={drawerState} />}
+      drawerState={drawerState}
+      setDrawerState={setDrawerState}
+    >
       <HomePage></HomePage>
     </DrawerMenu>
   );
